66c12c3b597151413ffbba5da8babfcab1bfab1d
Ray-popo <Ma3092353>
2020-01-27 19:18:40 -0800

    src

--- src/Practice01MathRecursive.java
+++ src/Practice01MathRecursive.java
@@ -12,22 +12,22 @@ public class Practice01MathRecursive implements Practice01Math {
 
     @Override
     public int fib(int n) throws Exception {
-        if(n==0) {
+        /*if(n==0) {
             return 0;
         } else if(n==1){
             return 1;
         } else if(n<0){
             return fib(n+2)+fib(n+1);
         }
-        return fib(n-1)+fib(n-2);
+        return fib(n-1)+fib(n-2);*/
 
-        /*if(n==0) {
+        if(n==0) {
             return 0;
         } else if(n==1){
             return 1;
         }
         return fib(n-1)+fib(n-2);
 
-         */
+
     }
 }
